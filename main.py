531d7372856befa8e4a01f388aa3965492605121
Zeke16 <kr2000.16@gmail.com>
2023-07-30 08:21:08 -0600

    Delete function

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ async def conn():
 def welcome():
     return "Bienvenido a la app"
 
+
 @app.get("/developers")
-async def dev():
+async def get_developers():
     try:
         db = await conn()
         developers = await db.developers.find().to_list(1000)
@@ -44,15 +45,42 @@ async def create_developer(developer: Developer):
         return JSONResponse(status_code=500, content={"message": "Error al registrar"})
 
 
-@app.put("/developers")
+@app.put("/developers/{id}")
 async def update_developer(developer: Developer, id: str):
     try:
         db = await conn()
         developers = await db.developers.find_one({"_id": ObjectId(id)})
         if not developers:
-            return JSONResponse(status_code=400, content={f"message": "registro con id ${id} no existe"})
+            return JSONResponse(status_code=400, content={"message": f"registro con id {id} no existe"})
 
         await db.developers.update_one({"_id": ObjectId(id)}, {"$set": jsonable_encoder(developer)})
         return JSONResponse(status_code=203, content={"message": "Registro actualizado"})
     except Exception as err:
         return JSONResponse(status_code=500, content={"message": "Error al actualizar"})
+
+
+@app.delete("/developers/{id}")
+async def delete_developer(id: str):
+    try:
+        db = await conn()
+        developers = await db.developers.find_one({"_id": ObjectId(id)})
+        if not developers:
+            return JSONResponse(status_code=400, content={"message": f"registro con id {id} no existe"})
+        await db.developers.delete_one({"_id": ObjectId(id)})
+        return JSONResponse(status_code=201, content={"message": "Registro eliminado"})
+    except Exception as err:
+        return JSONResponse(status_code=500, content={"message": "Error al eliminar"})
+
+
+@app.get("/developers/{id}")
+async def get_one_developer(id: str):
+    try:
+        db = await conn()
+        developers = await db.developers.find_one({"_id": ObjectId(id)})
+        if not developers:
+            return JSONResponse(status_code=400, content={"message": f"registro con id {id} no existe"})
+        developers["_id"] = str(developers["_id"])
+        return JSONResponse(status_code=200, content={"data": developers})
+    except Exception as err:
+        print(err)
+        return JSONResponse(status_code=500, content={"error": err})
